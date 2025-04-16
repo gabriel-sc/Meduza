3f3620431c36b2b131069b180fe45714913ec122
gabriel-sc <contacsgogabe@gmail.com>
2025-04-16 20:11:43 -0300

    Adicionado novo arquivo Gabriel Fluxo de cadastro

---
+++ Gabriel.js
@@ -0,0 +1,4 @@
+alert ("iniciando fluxo de cadastro de cliente")
+let name = prompt ("informe o nome")
+let CpfCnpj = prompt ("informe o CPF ou CNPJ")
+let cidadeResidencia = prompt("informe a cidade qual reside")
